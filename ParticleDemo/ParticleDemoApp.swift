b132b2d1f7f72884888d508a74f0fc0c2f698935
FaiChou <faichou.zh@gmail.com>
2024-11-14 19:58:17 +0800

    Initial Commit

---
+++ ParticleDemo/ParticleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleDemoApp.swift
+//  ParticleDemo
+//
+//  Created by 周辉 on 2024/11/14.
+//
+
+import SwiftUI
+
+@main
+struct ParticleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
